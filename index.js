4116e8b739ff907f6a78feeb07121933a32a1342
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-08-18 11:11:44 +0200

    start of deployment for testing only

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-server.listen(3500, () => {
-  console.log('server running on http://localhost:3500')
-
+const port = process.env.PORT || 3500;
+server.listen(port, () => {
+  console.log(`\n=== Server listening on port ${port}\n`);
 });
